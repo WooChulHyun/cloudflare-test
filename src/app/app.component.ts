f3594848e1f77550442b3de0468d553705db97b5
Woochul Hyun <woochul.hyun91@gmail.com>
2022-06-22 15:08:16 +0900

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {SEOService} from "./core/seo.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,15 @@ import {SEOService} from "./core/seo.service";
 export class AppComponent {
   title = 'codetebah';
 
-  constructor(private SEOService: SEOService) {
+  constructor(private SEOService: SEOService, private router: Router) {
+  }
+
+  toHome() {
+    this.router.navigate(['/home'])
+  }
+
+  toAbout() {
+    this.router.navigate(['/about'])
   }
 }
+
